5d3115cf398e947663b446a8bd4e937f7456d2a6
evazhog <evanzhong10@gmail.com>
2019-05-08 23:05:40 -0700

    more

--- src/server.js
+++ src/server.js
@@ -78,7 +78,12 @@ passport.use(new GoogleStrategy(
   },
   (accessToken, refreshToken, profile, done) => {
     console.log(profile)
-    return done()
+    if (profile._json.hd == 'ausdk12.org' && email_verified) {
+      return done()
+    }
+    else {
+      console.log("Not ausdk12")
+    }
     // User.findOrCreate({googleId: profile.id}, (err, user) => {
     //   return done(err, user);
     // });

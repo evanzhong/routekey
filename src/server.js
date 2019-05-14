da50ea966172834a8fc203c80812ab2a8187068a
evazhog <evanzhong10@gmail.com>
2019-05-14 09:30:56 -0700

    testing sending info to backend

--- src/server.js
+++ src/server.js
@@ -167,6 +167,7 @@ app.post('/new-route', (req, res) => {
 
 app.post('/sign-in-verify', (req, res) => {
   console.log(req.body.id_token)
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
 // Local testing

92a2e0c5ee32d8bac9236181aba1296508d9eaf7
evazhog <evanzhong10@gmail.com>
2019-05-14 10:14:54 -0700

    testing master

--- src/server.js
+++ src/server.js
@@ -74,7 +74,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.clientID,
     clientSecret: process.env.clientSecret,
-    callbackURL: "http://www.routekey.me/auth/google/admin"
+    callbackURL: "auth/google/admin"
   },
   (accessToken, refreshToken, profile, done) => {
     console.log(profile)

f2b8b45ead5988ba78af3e133ae8cd6aaaaea80a
evazhog <evanzhong10@gmail.com>
2019-09-16 19:38:13 -0700

    modal v0

--- src/public/main.js
+++ src/public/main.js
@@ -1,6 +1,32 @@
 const form = document.querySelector('form');
 const result = document.querySelector('.generated');
 
+function initModal() {
+  const modalCTA = document.querySelector('#modal-cta')
+  const modal = document.querySelector('#modal')
+  const modalBackdrop = document.querySelector('#modal-backdrop')
+  const modalWindow = document.querySelector('#modal-window')
+  const modalKey = document.querySelector('#modal-key')
+  modalKey.innerHTML = document.querySelector('#key').innerHTML
+
+  modalCTA.addEventListener('click', event => {
+    event.preventDefault();
+    modal.style.display = 'block';
+  });
+  modalBackdrop.addEventListener('click', event => {
+    event.preventDefault();
+    let targetBackground = true;
+    console.log('before enter')
+    modalWindow.addEventListener('click', event => {
+      event.preventDefault;
+      targetBackground = false;
+      console.log('enter')
+    });
+    console.log('after enter')
+    if (targetBackground) modal.style.display = 'none';
+  });
+}
+
 form.addEventListener('submit', event => {
   event.preventDefault();
   const input = document.querySelector('.url');
@@ -24,7 +50,10 @@ form.addEventListener('submit', event => {
       console.log(JSON.stringify(body));
       result.insertAdjacentHTML('afterbegin', `
         <div class="generated-wrapper">
-          <p id="notif">Your generated RouteKey is: <b>${body.key}</b></p>
+          <p id="notif">
+            Your generated RouteKey is: <b id="key">${body.key}</b>
+            <button id="modal-cta" class="dim">⇧</button>
+          </p>
           <div id="notif-link">
             <span>Go to: </span>
             <a target="_blank" class="generated-url" rel="noopener" href="/${body.key}">
@@ -34,6 +63,7 @@ form.addEventListener('submit', event => {
           </div>
         </div>
       `)
+      initModal();
       if (body.morris){
         document.querySelector('#notif-link').insertAdjacentHTML('afterend', `
           <div id="morrisism-congrats">
